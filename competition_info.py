a2ccc2776bdff9b13e397698a61d1851cb9fccdb
SPoovathumkadavil <97705117+SPoovathumkadavil@users.noreply.github.com>
2022-02-21 21:21:34 -0600

    Bruh I spent 6+ hours on this and the main function(competition_info) don't even work lmao.
    kill me

--- competition_info.py
+++ competition_info.py
@@ -1,116 +1,147 @@
 import tbapy as t
 import event_info
+import csv
 
 competition_teams = [
-111,
-648,
-706,
-1625,
-1716,
-1736,
-1756,
-1781,
-2039,
-2040,
-2338,
-2358,
-2451,
-2481,
-2704,
-2783,
-3061,
-3067,
-3381,
-3695,
-4096,
-4143,
-4213,
-4241,
-4500,
-4531,
-4645,
-4655,
-5442,
-5822,
-6651,
-7103,
-7417,
-7619,
-7848,
-8029,
-8096,
-8122,
-931,
-1178,
-1288,
-1329,
-1444,
-1658,
-1706,
-1736,
-1756,
-1781,
-1985,
-2408,
-2481,
-2707,
-2978,
-3397,
-3792,
-4143,
-4187,
-4232,
-4256,
-4329,
-4330,
-4500,
-4600,
-4931,
-5060,
-5176,
-5583,
-6157,
-6237,
-6391,
-6744,
-7117,
-7472,
-7496,
-7747,
-8069,
-8077,
-8719
+    111,
+    648,
+    706,
+    1625,
+    1716,
+    1736,
+    1756,
+    1781,
+    2039,
+    2040,
+    2338,
+    2358,
+    2451,
+    2481,
+    2704,
+    2783,
+    3061,
+    3067,
+    3381,
+    3695,
+    4096,
+    4143,
+    4213,
+    4241,
+    4500,
+    4531,
+    4645,
+    4655,
+    5442,
+    5822,
+    6651,
+    7103,
+    7417,
+    7619,
+    7848,
+    8029,
+    8096,
+    8122,
+    931,
+    1178,
+    1288,
+    1329,
+    1444,
+    1658,
+    1706,
+    1736,
+    1756,
+    1781,
+    1985,
+    2408,
+    2481,
+    2707,
+    2978,
+    3397,
+    3792,
+    4143,
+    4187,
+    4232,
+    4256,
+    4329,
+    4330,
+    4500,
+    4600,
+    4931,
+    5060,
+    5176,
+    5583,
+    6157,
+    6237,
+    6391,
+    6744,
+    7117,
+    7472,
+    7496,
+    7747,
+    8069,
+    8077,
+    8719
 ]
+competition_teams.sort()
 
-tba = t.TBA('import tbapy')
-tba = t.TBA('TjUTfbPByPvqcFaMdEQVKPsd8R4m2TKIVHMoqf3Vya0kAdqx3DlwDQ5Sly4N2xJS')
+def get_competition_info(year):
+    tba = t.TBA('import tbapy')
+    tba = t.TBA('TjUTfbPByPvqcFaMdEQVKPsd8R4m2TKIVHMoqf3Vya0kAdqx3DlwDQ5Sly4N2xJS')
 
-team_info = []
+    tmp_events = []
+    for i in competition_teams:
+        tmp_events.append(tba.team_events(i, year))
 
-for i in competition_teams:
-    info = tba.team(i)
-    team_info.append(info)
+    events = []
+    for i in tmp_events:
+        for j in i:
+            keeper = []
+            if j['key'] not in events:
+                keeper.append(j['key'])
+                keeper.append(j['name'])
+            events.append(keeper)
 
-competition_cities = set()
-for i in team_info:
-    competition_cities.add(i['city'])
+    # For each competition event, iterate once
+    with open('competition_info_output.csv', 'w', newline="") as output:
+        writer = csv.writer(output)
 
-competition_cities = list(competition_cities)
+        vals = [
+            ['competition', 'dq', 'extra_stats', 'matches_played', 'qualifying_average', 'rank', 'record', 'sort_orders', 'team_key']
+        ]
 
-print(competition_cities)
+        # For each competition event iterate once
+        for i in events:
+            er = event_info.get_event_info(i)
+            if not er:
+                continue
 
-print("\n")
+            # For each team, iterate once
+            times = 0
+            for j in er:
+                if times == 0:
+                    times += 1
+                    continue
+                CurrentKey = j[8]
 
-events = tba.events(2022)
-for i in events:
-    print(i)
+                num = ""
+                for e in CurrentKey:
+                    if e == 'f':
+                        continue
+                    elif e == 'r':
+                        continue
+                    elif e == 'c':
+                        continue
+                    else:
+                        num += e
+                num = int(num)
 
-print("\n")
+                for team in competition_teams:
+                    if num < team:
+                        break
+                    TeamKey = "frc" + str(team)
+                    if CurrentKey == TeamKey:
+                        vals.append(j)
+        writer.writerows(vals)
 
-competition_events = []
-for i in events:
-    if i['city'] in competition_cities:
-        competition_events.append(i['key'])
-
-for i in competition_events:
-    print(i)
+    output.close()
+    return vals
--- event_info.py
+++ event_info.py
@@ -5,22 +5,29 @@ tba = tbapy.TBA('import tbapy')
 
 tba = tbapy.TBA('TjUTfbPByPvqcFaMdEQVKPsd8R4m2TKIVHMoqf3Vya0kAdqx3DlwDQ5Sly4N2xJS')
 
-def get_event_info(event_key):
-    x = tba.event_rankings(event_key)
 
-    fieldnames = ['dq', 'extra_stats', 'matches_played', 'qual_average', 'rank', 'record', 'sort_orders', 'team_key']
+def get_event_info(kl):
+    try:
+        x = tba.event_rankings(kl[0])['rankings']
+    except KeyError:
+        return False
+    keys = x[len(x)-1].keys()
 
-    with open('output.csv', 'w', newline="") as output:
+    with open('event_info_output.csv', 'w', newline="") as output:
         writer = csv.writer(output)
 
-        vals = [['dq', 'extra_stats', 'matches_played', 'qual_average', 'rank', 'record', 'sort_orders', 'team_key']]
-        for i in x["rankings"]:
-            t = []
-            for n in i:
-                val = i[n]
-                t.append(val)
+        vals = [['competition', 'dq', 'extra_stats', 'matches_played', 'qual_average', 'rank', 'record', 'sort_orders', 'team_key']]
 
-            vals.append(t)
-        writer.writerows(vals)
+        if x is not None:
+            for i in x:
+                t = [kl[1]]
+                for j in keys:
+                    t.append(i[j])
+
+                vals.append(t)
+            writer.writerows(vals)
+        else:
+            return False
 
     output.close()
+    return vals
